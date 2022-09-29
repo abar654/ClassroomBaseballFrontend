19346b7722962321c12fe240d8e9445e5eac9e84
abar654 <andrew.barnett.au@gmail.com>
2022-09-29 13:42:05 +1000

    Set up stats service with award functions. Hooked up TeamAward component to stats service.

--- src/app/teams/team-statistics/team-statistics.component.ts
+++ src/app/teams/team-statistics/team-statistics.component.ts
@@ -3,6 +3,11 @@ import { Subscription } from "rxjs";
 import { Scorecard } from "src/app/scorecards/models/scorecard.model";
 import { TeamStatisticsService } from "./team-statistics.service";
 
+interface Award {
+    title: string,
+    recipient: string
+}
+
 /**
  * A panel component for displaying statistics about the most recent week/games.
  */
@@ -14,25 +19,59 @@ import { TeamStatisticsService } from "./team-statistics.service";
 })
 export class TeamStatisticsComponent implements OnInit, OnDestroy {
 
-    rankedWeekScores: Scorecard[] = [];
+    private readonly BFAWARD: string = "Best and fairest";
+    private readonly MIAWARD: string = "Most improved";
 
-    private weekScoresStateSub: Subscription;
+    rankedWeekScores: Scorecard[] = [];
+    awards: Award[] = [
+        {
+            title: this.BFAWARD,
+            recipient: ""
+        },
+        {
+            title: this.MIAWARD,
+            recipient: ""
+        }
+    ];
+
+    private subscriptions: Subscription[] = [];
 
     constructor(
         private teamStatisticsService: TeamStatisticsService
     ){}
 
     ngOnInit(): void {
-        this.weekScoresStateSub = this.teamStatisticsService.getRankedWeekScoresState()
-            .subscribe((weekScores: Scorecard[]) => {
+        this.subscriptions = [
+            this.teamStatisticsService.getRankedWeekScoresState().subscribe((weekScores: Scorecard[]) => {
                 if (weekScores != null) {
                     this.rankedWeekScores = weekScores;
                 }
-            });
+            }),
+            this.teamStatisticsService.getBestAndFairestState().subscribe((recipient: string) => {
+                this.setAwardRecipient(this.BFAWARD, recipient);
+            }),
+            this.teamStatisticsService.getMostImprovedState().subscribe((recipient: string) => {
+                this.setAwardRecipient(this.MIAWARD, recipient);
+            })
+        ];
     }
 
     ngOnDestroy(): void {
-        this.weekScoresStateSub && this.weekScoresStateSub.unsubscribe();
+        this.subscriptions.forEach((subscription: Subscription) => {
+            subscription.unsubscribe();
+        });
+    }
+
+    private setAwardRecipient(title: string, recipient: string): void {
+        this.awards = this.awards.map((award: Award) => {
+            if (award.title === title) {
+                return {
+                    title,
+                    recipient
+                }
+            }
+            return award;
+        });
     }
 
 }
--- src/app/teams/team-statistics/team-statistics.service.ts
+++ src/app/teams/team-statistics/team-statistics.service.ts
@@ -15,6 +15,8 @@ import { TeamsService } from "../teams.service";
     private readonly WEEK_DURATION: number = 7 * 24 * 60 * 60 * 1000;
 
     private rankedWeekScores: BehaviorSubject<Scorecard[]> = new BehaviorSubject<Scorecard[]>([]);
+    private bestAndFairest: BehaviorSubject<string> = new BehaviorSubject<string>("");
+    private mostImproved: BehaviorSubject<string> = new BehaviorSubject<string>("");
 
     constructor(
         private teamsService: TeamsService,
@@ -26,12 +28,16 @@ import { TeamsService } from "../teams.service";
                 const startTimestamp: number = this.getStartOfWeekTimestamp(new Date().getTime() - this.WEEK_DURATION);
                 this.statisticsApi.getWeekScores(team.id, startTimestamp).subscribe(
                     (weekScores: Scorecard[]) => {
+                        // Broadcast the sorted week scores
                         this.rankedWeekScores.next(weekScores.sort((a, b) => {
                             if (a.bases === b.bases) {
                                 return a.strikes - b.strikes;
                             }
                             return b.bases - a.bases;
                         }));
+
+                        this.bestAndFairest.next(this.getBestAndFairest(weekScores));
+                        this.mostImproved.next(this.getMostImproved(weekScores, startTimestamp))
                     },
                     (error: any) => {
                         console.log("loadStatistics - error: ", error);
@@ -43,6 +49,31 @@ import { TeamsService } from "../teams.service";
 
     getRankedWeekScoresState(): BehaviorSubject<Scorecard[]> {
         return this.rankedWeekScores;
+    }
+
+    getBestAndFairestState(): BehaviorSubject<string> {
+        return this.bestAndFairest;
+    }
+
+    getMostImprovedState(): BehaviorSubject<string> {
+        return this.mostImproved;
+    }
+
+    /**
+     * Calculates the best and fairest player and returns their name.
+     * Best and fairest is the player with the greatest "bases - strikes" score.
+     */
+    private getBestAndFairest(weekScores: Scorecard[]): string {
+        return "Thing 1";
+    }
+
+    /**
+     * Calculates the most improved player and returns their name.
+     * Most improved is the player with the greatest increase in their "bases - strikes" score
+     * from 2 weeks ago to last week.
+     */
+    private getMostImproved(weekScores: Scorecard[], lastWeekTimestamp: number): string {
+        return "Thing 2";
     }
 
     /**
