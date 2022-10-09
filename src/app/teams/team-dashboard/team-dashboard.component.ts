7eb65fff4668e4aa87b0cc22fd215db9371a0e43
abar654 <andrew.barnett.au@gmail.com>
2022-10-10 10:15:02 +1100

    Disable continue game button when no loaded game.

--- src/app/teams/team-dashboard/team-dashboard.component.ts
+++ src/app/teams/team-dashboard/team-dashboard.component.ts
@@ -21,11 +21,11 @@ import { TeamsService } from "../teams.service";
 export class TeamDashboardComponent implements OnInit, OnDestroy {
 
     teamData: Team = null;
+    mostRecentGame: Game = null;
     recentRankedScorecards: Scorecard[] = [];
     isEditing: boolean = false;
 
-    private currentTeamStateSub: Subscription;
-    private recentScoresSub: Subscription;
+    subscriptions: Subscription[] = [];
 
     constructor(
         private route: ActivatedRoute,
@@ -42,44 +42,49 @@ export class TeamDashboardComponent implements OnInit, OnDestroy {
             await this.teamsService.loadTeams();
             this.teamsService.setCurrentTeamById(parseInt(params.teamId));
 
-            this.currentTeamStateSub = this.teamsService.getCurrentTeamState().subscribe((team: Team) => {
+            this.subscriptions = [
+                this.teamsService.getCurrentTeamState().subscribe((team: Team) => {
 
-                // If teams is still null then data hasn't been loaded yet.
-                const teams = this.teamsService.getTeamsState().getValue();
-                if (teams !== null) {
-
-                    this.teamData = team;
-                    if (this.teamData !== null) {
-
-                        this.headerService.setTitle(this.teamData.name);
-
-                        // Load the most recent game, if a game exists.
-                        if (this.teamData.games.length > 0) {
-                            this.gamesService.loadGame(
-                                this.teamData.id, 
-                                this.teamData.games[this.teamData.games.length - 1].id
-                            );
+                    // If teams is still null then data hasn't been loaded yet.
+                    const teams = this.teamsService.getTeamsState().getValue();
+                    if (teams !== null) {
+    
+                        this.teamData = team;
+                        if (this.teamData !== null) {
+    
+                            this.headerService.setTitle(this.teamData.name);
+    
+                            // Load the most recent game, if a game exists.
+                            if (this.teamData.games.length > 0) {
+                                this.gamesService.loadGame(
+                                    this.teamData.id, 
+                                    this.teamData.games[this.teamData.games.length - 1].id
+                                );
+                            } else {
+                                this.gamesService.unloadGame();
+                            }
+    
                         } else {
-                            this.gamesService.unloadGame();
+                            this.router.navigate(['/teams']);
                         }
-
-                    } else {
-                        this.router.navigate(['/teams']);
+    
                     }
-
-                }
-
-            });
-
-            this.recentScoresSub = this.gamesService.getRankedScorecardsState().subscribe((scorecards: Scorecard[]) => {
-                this.recentRankedScorecards = scorecards;
-            });
+    
+                }),
+                this.gamesService.getLoadedGameState().subscribe((game: Game) => {
+                    this.mostRecentGame = game;
+                }),
+                this.gamesService.getRankedScorecardsState().subscribe((scorecards: Scorecard[]) => {
+                    this.recentRankedScorecards = scorecards;
+                })
+            ];
         });
     }
 
     ngOnDestroy(): void {
-        this.currentTeamStateSub && this.currentTeamStateSub.unsubscribe();
-        this.recentScoresSub && this.recentScoresSub.unsubscribe();
+        this.subscriptions.forEach((subscription: Subscription) => {
+            subscription.unsubscribe();
+        });
     }
 
     async startNewGame() {
@@ -96,9 +101,8 @@ export class TeamDashboardComponent implements OnInit, OnDestroy {
     }
 
     continueMostRecentGame() {
-        const mostRecentGame = this.gamesService.getLoadedGameState().value;
-        if (this.teamData && mostRecentGame) {
-            this.router.navigate(['/teams/' + this.teamData.id + '/games/' + mostRecentGame.id]);
+        if (this.teamData && this.mostRecentGame) {
+            this.router.navigate(['/teams/' + this.teamData.id + '/games/' + this.mostRecentGame.id]);
         }
     }
 
