16caca9ab9742669f9e2120d9b1a10926917403a
abar654 <andrew.barnett.au@gmail.com>
2022-10-09 13:17:35 +1100

    Changes for production building.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiEndpoint: 'http://classroombaseball.net/api',
+  authenticatedRedirect: '/teams',
+  logoutRedirect: ''
 };
